ae6fa87f1997ca40a5f10fc5fdf3cd20bf937b11
Brent Tubbs <brent.tubbs@gmail.com>
2015-06-22 15:04:55 -0600

    optional tracebacks in json exceptions

--- spa/exceptions.py
+++ spa/exceptions.py
@@ -1,4 +1,7 @@
 import json
+import traceback
+
+from werkzeug.exceptions import HTTPException
 
 from werkzeug import exceptions
 
@@ -6,12 +9,21 @@ from werkzeug import exceptions
 # Some freestanding methods for feeding into our dynamically-constructed
 # exception classes later.  They override the corresponding HTTPException
 # methods to produce JSON output.
+
+def init(self, description=None, response=None, traceback=None):
+    self.traceback = traceback
+    HTTPException.__init__(self, description, response)
+
 def get_body(self, environ=None):
-    return json.dumps(dict(
+    data = dict(
         code=self.code,
         name=self.name,
         description=self.description,
-    ))
+    )
+    if self.traceback:
+        tb = traceback.format_list(traceback.extract_tb(self.traceback))
+        data['traceback'] = tb
+    return json.dumps(data)
 
 def get_headers(self, environ=None):
     return [('Content-Type', 'application/json')]
@@ -26,5 +38,6 @@ for obj_name in dir(exceptions):
         cls = type(new_cls_name, (obj,), dict(
             get_body=get_body,
             get_headers=get_headers,
+            __init__=init,
         ))
         globals()[new_cls_name] = cls

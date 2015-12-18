6e3df5f5000432b30d63ff889ad449f58023c0f1
Brent Tubbs <brent.tubbs@gmail.com>
2015-12-18 15:29:20 -0700

    clean up JSONHandler inheritance

--- spa/handler.py
+++ spa/handler.py
@@ -20,22 +20,28 @@ class Handler(object):
     def websocket_close(self):
         pass
 
-    def __call__(self, environ, start_response):
-        if self.request.method not in self.allowed_methods:
-            return MethodNotAllowed()(environ, start_response)
-
+    def _get_handler(self):
+        environ = self.request.environ
         if self.request.method == 'GET' and 'wsgi.websocket' in environ:
             self.ws = environ['wsgi.websocket']
             self.ws.add_close_callback(self.websocket_close)
 
-            handler = self.websocket
-        else:
-            handler = getattr(self, self.request.method.lower())
+            return self.websocket
+        return getattr(self, self.request.method.lower())
+
+    def _get_response(self, environ, start_response):
+        if self.request.method not in self.allowed_methods:
+            return MethodNotAllowed()(environ, start_response)
+
+        handler = self._get_handler()
 
         if handler == NotImplemented:
             return MethodNotAllowed()
 
-        resp = handler(**self.params)
+        return handler(**self.params)
+
+    def __call__(self, environ, start_response):
+        resp = self._get_response(environ, start_response)
         return resp(environ, start_response)
 
 
@@ -43,24 +49,10 @@ class JSONHandler(Handler):
     """Works the same as Handler, but allows you to reply with just a dict that
     will then be turned into a JSONResponse for you."""
 
-    def _get_response(self):
-        environ = self.request.environ
 
-        if self.request.method == 'GET' and 'wsgi.websocket' in environ:
-            self.ws = environ['wsgi.websocket']
-            self.ws.add_close_callback(self.websocket_close)
-
-            handler = self.websocket
-        else:
-            handler = getattr(self, self.request.method.lower())
-
-        resp = handler(**self.params)
+    def __call__(self, environ, start_response):
+        resp = self._get_response(environ, start_response)
 
         if isinstance(resp, dict):
             resp = JSONResponse(resp)
-        return resp
-
-    def __call__(self, environ, start_response):
-        if self.request.method not in self.allowed_methods:
-            return MethodNotAllowed()(environ, start_response)
-        return self._get_response()(environ, start_response)
+        return resp(environ, start_response)
